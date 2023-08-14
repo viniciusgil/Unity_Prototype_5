a81de3eb34fd2347d8707e7d98bf4ed5bae9f4fc
viniciusgil <viniciusgil_es@hotmail.com>
2023-08-14 11:34:10 +0100

    Adding difficulty

--- Prototype 5/Assets/Script/GameManager.cs
+++ Prototype 5/Assets/Script/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
@@ -18,13 +19,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
-
-      
+            
         
     }
 
@@ -60,5 +55,16 @@ public class GameManager : MonoBehaviour
     {
         
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+    public void StartGame(int  difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
